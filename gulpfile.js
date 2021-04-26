6926594c48306cc24bcb2d05f8998d978faf7e32
Алёна Царегородцева <aileen.kingsler@mail.ru>
2021-04-26 16:50:04 +1000

    Вёрстка блока Reviews

--- gulpfile.js
+++ gulpfile.js
@@ -55,6 +55,7 @@ gulp.task("scripts", function () {
     gulp.src([
       "node_modules/jquery/dist/jquery.min.js",
       "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
+      "node_modules/owl.carousel/dist/owl.carousel.min.js",
       "source/js/scripts.js"
       ]),
     concat("scripts.min.js"),
